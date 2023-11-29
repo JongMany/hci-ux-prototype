f04a5adb0f65ed16e3ab5acd9d18f988cccc07b9
JongMany <blackberry1114@naver.com>
2023-11-30 01:42:43 +0900

    단어 변경

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,16 +1,15 @@
-import Loading from "./Loading";
-import {useEffect, useState} from "react";
-import {answers} from "../help/answer";
+import Loading from './Loading';
+import { useEffect, useState } from 'react';
+import { answers } from '../help/answer';
 import styles from './Answer.module.css';
-import {aiType} from "../help/aiType";
-import {keywords} from "../help/keywords";
-import {texts} from "../help/textAnswers";
+import { aiType } from '../help/aiType';
+import { keywords } from '../help/keywords';
+import { texts } from '../help/textAnswers';
 
-
-const Answer = ({question, num, type}) => {
+const Answer = ({ question, num, type }) => {
   const localAnswers = JSON.parse(localStorage.getItem('answers' + type));
   const [isVisible, setIsVisible] = useState(true);
-  const localAnswer = localAnswers ? localAnswers[num]?.answer : ''
+  const localAnswer = localAnswers ? localAnswers[num]?.answer : '';
   const [answer, setAnswer] = useState(localAnswer);
 
   useEffect(() => {
@@ -23,34 +22,38 @@ const Answer = ({question, num, type}) => {
     if (aiType.textImage) time = 2500;
     if (aiType.textImage3) time = 4500;
     setTimeout(() => {
-      const keywords = hasKeywordsInSentence(question)
-
+      const keywords = hasKeywordsInSentence(question);
+      console.log(keywords);
       if (keywords.length) {
         // 키워드에 맞는 답변을 세팅
         const aiAnswer = getAnswersByAI(type, keywords);
         // setAnswer(answers[num].answer);
-        console.log(aiAnswer)
-        setAnswer(aiAnswer)
+        console.log(aiAnswer);
+        setAnswer(aiAnswer);
       } else {
-        setAnswer({text: '학습되지 않은 키워드입니다...'});
+        setAnswer({ text: '학습되지 않은 키워드입니다...' });
       }
       setIsVisible(false);
     }, time);
   }, []);
 
-  return (<div className={styles['answer-section']}>
-        <div className={styles.icon}>AI:</div>
-        <div className={styles.answer}>
-          {!isVisible &&
-              answer.imgSrcs &&
-              answer.imgSrcs.map((src, idx) => <img key={idx} src={src} width='300'
-                                                    height='200'/>)}
-          <p style={{whiteSpace: 'pre-wrap', fontSize: '0.75rem'}}>{!isVisible && answer.text}</p>
-          <Loading isVisible={isVisible}/>
-        </div>
+  return (
+    <div className={styles['answer-section']}>
+      <div className={styles.icon}>AI:</div>
+      <div className={styles.answer}>
+        {!isVisible &&
+          answer.imgSrcs &&
+          answer.imgSrcs.map((src, idx) => (
+            <img key={idx} src={src} width='300' alt={idx} height='200' />
+          ))}
+        <p style={{ whiteSpace: 'pre-wrap', fontSize: '0.75rem' }}>
+          {!isVisible && answer.text}
+        </p>
+        <Loading isVisible={isVisible} />
       </div>
-  )
-}
+    </div>
+  );
+};
 
 function hasKeywordsInSentence(sentence) {
   const results = [];
@@ -63,32 +66,46 @@ function hasKeywordsInSentence(sentence) {
 }
 
 function getAnswersByAI(type, keywords) {
-  const results = {}
+  const results = {};
   const keyword = getKeyword(keywords);
+  let num = 0;
+  if (type === aiType.text) num = 0;
+  if (type === aiType.textImage) num = 1;
+  if (type === aiType.textImage3) num = 2;
+
   if (type === aiType.textImage) {
-    results.imgSrcs = [`${process.env.PUBLIC_URL}/assets/images/${keyword}/1.png`];
+    results.imgSrcs = [
+      `${process.env.PUBLIC_URL}/assets/images/${keyword}/1.png`,
+    ];
   } else if (type === aiType.textImage3) {
-    results.imgSrcs = [`${process.env.PUBLIC_URL}/assets/images/${keyword}/2.png`, `${process.env.PUBLIC_URL}/assets/images/${keyword}/3.png`, `${process.env.PUBLIC_URL}/assets/images/${keyword}/4.png`];
+    results.imgSrcs = [
+      `${process.env.PUBLIC_URL}/assets/images/${keyword}/2.png`,
+      `${process.env.PUBLIC_URL}/assets/images/${keyword}/3.png`,
+      `${process.env.PUBLIC_URL}/assets/images/${keyword}/4.png`,
+    ];
   }
-  results.text = texts[keyword];
+  results.text = texts[keyword][num];
 
   return results;
 }
 
 function getKeyword(keywords) {
+  // ['레트로', '레트로 바이러스', '레트로바이러스', '압전효과', '압전 효과', '반추동물', '반추 동물', '트리핀 딜레마', '트리핀딜레마', '트리핀']
   // ['레일리 산란', '레일리산란', '리소좀', '블록체인', '자이가르닉', '페르소나', '자이가르닉 효과', '페르소나 효과', '자이가르닉효과', '페르소나효과']
-  if (keywords.includes('레일리'))
-    return '레일리산란';
-  else if (keywords.includes('리소좀'))
-    return '리소좀';
-  else if (keywords.includes('비트코인') || keywords.includes('블록체인') || keywords.includes('코인'))
-    return '블록체인';
-  else if (keywords.includes('자이가르닉'))
-    return '자이가르닉';
-  else if (keywords.includes('페르소나'))
-    return '페르소나';
-  else
-    return null;
+  console.log(keywords);
+  if (keywords.includes('레트로') || keywords.includes('레트로 바이러스'))
+    return '레트로바이러스';
+  else if (keywords.includes('압전효과') || keywords.includes('압전 효과'))
+    return '압전효과';
+  else if (
+    keywords.includes('반추 동물') ||
+    keywords.includes('반추동물') ||
+    keywords.includes('반추')
+  )
+    return '반추동물';
+  else if (keywords.includes('트리핀') || keywords.includes('트리핀딜레마'))
+    return '트리핀딜레마';
+  else return null;
 }
 
 export default Answer;
--- src/components/QuestionInput.jsx
+++ src/components/QuestionInput.jsx
@@ -1,13 +1,12 @@
-import {useState} from "react";
+import { useState } from 'react';
 import styles from './QuestionInput.module.css';
 
-
-const QuestionInput = ({setQuestionList, questionList, type}) => {
+const QuestionInput = ({ setQuestionList, questionList, type }) => {
   const [question, setQuestion] = useState('');
 
   const changeHandler = (e) => {
     setQuestion(e.target.value);
-  }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -17,15 +16,23 @@ const QuestionInput = ({setQuestionList, questionList, type}) => {
     setQuestion('');
     localStorage.removeItem('question' + type);
     localStorage.setItem('question' + type, JSON.stringify(questions));
-  }
+  };
 
-  return (<section className={styles['question-container']}>
-    <form onSubmit={submitHandler}>
-      <input onChange={changeHandler} value={question} placeholder='질문을 입력해보세요'
-             className={styles['question-input']}/>
-      <button type='submit' className={styles['submit-btn']}>제출</button>
-    </form>
-  </section>)
-}
+  return (
+    <section className={styles['question-container']}>
+      <form onSubmit={submitHandler}>
+        <input
+          onChange={changeHandler}
+          value={question}
+          placeholder='질문을 입력해보세요'
+          className={styles['question-input']}
+        />
+        <button type='submit' className={styles['submit-btn']}>
+          제출
+        </button>
+      </form>
+    </section>
+  );
+};
 
 export default QuestionInput;
